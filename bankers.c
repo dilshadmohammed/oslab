b7f2a6714f8cb5bdeb31e1899b8775058fb6c912
dilshadmohammed <dilshadmohammed13@gmail.com>
2024-07-07 12:26:09 +0530

    fix: bankers algorithm

--- bankers.c
+++ bankers.c
@@ -1,94 +1,96 @@
 #include <stdio.h>
-int allocation[10][10],max[10][10],need[10][10],available[10],finish[10],sq[10];
-int nr,np;
 
-void readMatrix(int matrix[10][10]){
-    for(int i=0;i<np;i++){
-        for(int j=0;j<nr;j++){
-            scanf("%d",&matrix[i][j]);
+int n, m;
+
+void readMatrix(int matrix[n][m])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &matrix[i][j]);
         }
     }
 }
 
-void printMatrix(int matrix[10][10]){
-    for(int i=0;i<np;i++){
-        for(int j=0;j<nr;j++){
-            printf("%d ",matrix[i][j]);
-        }
-        printf("\n");
-    }
+void addToAvailable(int available[], int released[])
+{
+    for (int i = 0; i < m; i++)
+        available[i] += released[i];
 }
 
-void calculate_need(){
-    for(int i=0;i<np;i++){
-        for(int j=0;j<nr;j++){
+int checkNeed(int need[], int available[])
+{
+    for (int i = 0; i < m; i++)
+        if (need[i] > available[i])
+            return 0;
+    return 1;
+}
+
+int main()
+{
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    printf("Enter number of resource types: ");
+    scanf("%d", &m);
+
+    int available[m];
+    int max[n][m];
+    int allocation[n][m];
+    int need[n][m];
+    int finish[n], safesq[n];
+    for (int i = 0; i < n; i++)
+        finish[i] = 0;
+
+    printf("Enter available resources:\n");
+    for (int i = 0; i < m; i++)
+        scanf("%d", &available[i]);
+
+    printf("Enter Max matrix:\n");
+    readMatrix(max);
+
+    printf("Enter allocation matrix:\n");
+    readMatrix(allocation);
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
             need[i][j] = max[i][j] - allocation[i][j];
         }
     }
-}
 
-void bankers(){
-    for(int i=0;i<np;i++)
-        finish[i]=0;
-    int k=0,iterations=0;
-    while(k<np && iterations<np){
-        iterations++;
-    
-        for(int i=0;i<np;i++){
-            int flag=0;
-            if(finish[i]==0){
-                for(int j=0;j<nr;j++){
-                    if(need[i][j]>available[j]){
-                        flag=1;
-                        break;
-                    }
-                }
-                if(flag==0){
-                    finish[i]=1;
-                    sq[k++] = i;
-                    
-                    for (int x = 0; x < nr; x++)
-                        available[x] += allocation[i][x];
-                    iterations=0;
-                }
+    int k = 0, progress = 1;
+    while (progress)
+    {
+        progress = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (!finish[i] && checkNeed(need[i], available))
+            {
+                addToAvailable(available, allocation[i]);
+                finish[i] = 1;
+                safesq[k++] = i;
+                progress = 1;
             }
         }
     }
 
-
-    int flag=0;
-    for(int i=0;i<np;i++){
-        if(finish[i]==0){
-            printf("System is unsafe\n");
-            flag = 1;
-            break;
+    for (int i = 0; i < n; i++)
+    {
+        if (!finish[i])
+        {
+            printf("\n\n<===System is in UNSAFE state===>\n");
+            return 0;
         }
     }
-    if(flag==0){
 
-    printf("Safe sequence: ");
-    for(int i=0;i<np;i++){
-        printf("P%d -> ",sq[i]+1);
+    printf("\n\n<===System is in SAFE state===>\n");
+    printf("\nSafe sequence: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf(" P%d ->", safesq[i]);
     }
-    }
-
-}
-
-
-int main(){
-    printf("Enter the number of resource types: ");
-    scanf("%d",&nr);
-    printf("Enter the number of processes: ");
-    scanf("%d",&np);
-    printf("Enter allocation matrix: \n");
-    readMatrix(allocation);
-    printf("Enter Max matrix: \n");
-    readMatrix(max);
-    calculate_need();
-    printf("Enter available resources: ");
-    for(int i=0;i<nr;i++){
-        scanf("%d",&available[i]);
-    }
-
-    bankers();
+    printf("\b\b  ");
 }
